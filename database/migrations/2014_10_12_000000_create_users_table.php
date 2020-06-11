a38267d530bd2ad2befb9a98ca77a9596e1a29ef
Arup <arupctg@gmail.com>
2020-06-11 23:42:03 +0600

    basic setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,14 +11,20 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up() :void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name',128);
+            $table->string('email',128)->unique();
+            $table->string('phone_number',32)->unique();
             $table->string('password');
+            $table->string('reward_points')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('email_verified')->default(0);
+            $table->string('email_verification_token',128)->nullable();
+            $table->string('facebook_id',32)->nullable();
+            $table->string('google_id',32)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
@@ -29,7 +35,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down() :void
     {
         Schema::dropIfExists('users');
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 6
+                    Rio Ecommerce
                 </div>
 
                 <div class="links">
