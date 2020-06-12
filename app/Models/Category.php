49ccb1f524cd6d55b7360260a218bd63e87a8425
Arup <arupctg@gmail.com>
2020-06-12 15:50:08 +0600

    Category table seeder add

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,11 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     //
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($category){
+             $category->slug = Str::slug($category->name);
+        });
+    }
+
 
     public function parent_category():\Illuminate\Database\Eloquent\Relations\BelongsTo
     {
---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() : void
+    {
+        factory(Category::class,10)->create();
+    }
+}
