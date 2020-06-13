f13c1cb67400c6bd9027c1d448c7a24e94e744cd
Arup <arupctg@gmail.com>
2020-06-13 22:07:56 +0600

    product factory update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'category_id' => Category::all()->random()->id,
-        'title' => $faker->text(100),
+        'title' => $faker->text(25),
         'description' => $faker->realText(),
         'price' => random_int(100,1000),
     ];
