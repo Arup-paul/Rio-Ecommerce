305ecb15636ac4302260ff3a2caaa0802883032d
Arup <arupctg@gmail.com>
2020-06-13 14:30:14 +0600

    laravel mix and larvel ui setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css/app.css')
+     .styles([
+         'resources/css/custom.css',
+     ],'public/css/custom.css')
+     .js([
+         'resources/js/app.js',
+         'resources/js/custom.js',
+     ],'public/js/all.js')
+     .version();
