67218dda75c22080c5604fcae0f9739f099ed892
Arup <arupctg@gmail.com>
2020-06-13 19:32:47 +0600

    master layout done

---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,11 @@
+
+          <footer class="text-muted">
+            <div class="container">
+              <p class="float-right">
+                <a href="#">Back to top</a>
+              </p>
+            <p> &copy; {{config('app.name')}} {{date('Y')}}</p>
+            <p>Read Our <a href="#">Privacy Policy</a> or <a href="#">Contact Us</a></p>
+              
+            </div>
+          </footer>
